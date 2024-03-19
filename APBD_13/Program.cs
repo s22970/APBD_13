b7487d6f70eab966a37e250595281c5dba9a7b6d
F0xywow <maciej.rebisz00@gmail.com>
2024-03-19 13:10:37 +0100

    Answer to average

--- APBD_13/Program.cs
+++ APBD_13/Program.cs
@@ -1,5 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Modyfikacja 1");
-Console.WriteLine("Modyfikacja 2");
-Console.WriteLine("Modyfikacja 3");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia: " + average);
+    }
+
+    static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica jest pusta lub null");
+        }
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
